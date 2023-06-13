9f15688092d012907b1df15fb5e76e7dee0239a1
Ken Chan <chankakit1996@yahoo.com.hk>
2023-06-14 02:35:18 +0800

    basic ui structure

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+import TheFooter from '@/components/TheFooter.vue';
+import TheNavbar from '@/components/TheNavbar.vue';
+import TheServices from '@/components/TheServices.vue';
+import TheTransactions from '@/components/TheTransactions.vue';
+import TheWelcome from '@/components/TheWelcome.vue';
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="min-h-screen">
+    <div class="gradient-bg-welcome">
+      <TheNavbar />
+      <TheWelcome />
+    </div>
+    <TheServices />
+    <TheTransactions />
+    <TheFooter />
+  </div>
 </template>
